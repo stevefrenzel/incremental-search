b96a0901800e52fb00bb34bd4468ef322632f168
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-12 15:20:29 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,276 @@
+(function() {
+    
+    // USER INPUT
+
+    let results = $('#results');
+    let textfield = $('input');
+
+    textfield.on('input', function() {
+        let val = textfield.val();
+        let matches = [];
+        let resultsHTML = '';
+
+        if (val.length == 0) {
+            results.hide();
+            return;
+        }
+
+        for (let i = 0; i < countries.length; i++) {
+            if (countries[i].toLowerCase().indexOf(val.toLowerCase()) == 0) {
+                matches.push(countries[i]);
+                resultsHTML += '<div>' + countries[i] + '</div>';
+                if (matches.length == 4) {
+                    break;
+                }
+            }
+        }
+
+        if (matches.length == 0) {
+            resultsHTML += '<div>no results</div>';
+        }
+
+        $(results)
+            .html(resultsHTML)
+            .show();
+    });
+
+    // MOUSE MOVEMENT
+
+    results.on('mouseover', 'div', function(e) {
+        $('#results div').removeClass('on');
+        $(e.target).addClass('on');
+    });
+
+    results.on('mousedown', 'div', function(e) {
+        textfield.val($(e.target).html());
+        results.hide();
+    });
+
+    // ARRAY WITH COUNTRIES
+
+    const countries = [
+        "Afghanistan",
+        "Albania",
+        "Algeria",
+        "American Samoa",
+        "Angola",
+        "Anguilla",
+        "Antigua",
+        "Argentina",
+        "Armenia",
+        "Aruba",
+        "Australia",
+        "Austria",
+        "Azerbaijan",
+        "Bahamas",
+        "Bahrain",
+        "Bangladesh",
+        "Barbados",
+        "Belarus",
+        "Belgium",
+        "Belize",
+        "Benin",
+        "Bermuda",
+        "Bhutan",
+        "Bolivia",
+        "Bonaire (Netherlands Antilles)",
+        "Bosnia Herzegovina",
+        "Botswana",
+        "Brazil",
+        "British Virgin Islands",
+        "Brunei",
+        "Bulgaria",
+        "Burkina Faso",
+        "Burundi",
+        "Cambodia",
+        "Cameroon",
+        "Canada",
+        "Cape Verde",
+        "Cayman Islands",
+        "Central African Republic",
+        "Chad",
+        "Chile",
+        "China",
+        "Colombia",
+        "Comoros",
+        "Congo",
+        "Congo, The Democratic Republic of",
+        "Cook Islands",
+        "Costa Rica",
+        "Croatia",
+        "Curacao (Netherlands Antilles)",
+        "Cyprus",
+        "Czech Republic",
+        "Denmark",
+        "Djibouti",
+        "Dominica",
+        "Dominican Republic",
+        "Ecuador",
+        "Egypt",
+        "El Salvador",
+        "Equatorial Guinea",
+        "Eritrea",
+        "Estonia",
+        "Ethiopia",
+        "Fiji",
+        "Finland",
+        "France",
+        "French Guiana",
+        "French Polynesia",
+        "Gabon",
+        "Gambia",
+        "Georgia",
+        "Germany",
+        "Ghana",
+        "Gibraltar",
+        "Greece",
+        "Grenada",
+        "Guadeloupe",
+        "Guam",
+        "Guatemala",
+        "Guinea",
+        "Guinea Bissau",
+        "Guyana",
+        "Haiti",
+        "Honduras",
+        "Hong Kong",
+        "Hungary",
+        "Iceland",
+        "India",
+        "Indonesia",
+        "Iraq",
+        "Ireland (Republic of)",
+        "Israel",
+        "Italy",
+        "Ivory Coast",
+        "Jamaica",
+        "Japan",
+        "Jordan",
+        "Kazakhstan",
+        "Kenya",
+        "Kiribati",
+        "Kosovo",
+        "Kosrae Island",
+        "Kuwait",
+        "Kyrgyzstan",
+        "Laos",
+        "Latvia",
+        "Lebanon",
+        "Lesotho",
+        "Liberia",
+        "Libya",
+        "Lithuania",
+        "Luxembourg",
+        "Macau",
+        "Macedonia (FYROM)",
+        "Madagascar",
+        "Malawi",
+        "Malaysia",
+        "Maldives",
+        "Mali",
+        "Malta",
+        "Marshall Islands",
+        "Martinique",
+        "Mauritania",
+        "Mauritius",
+        "Mayotte",
+        "Mexico",
+        "Moldova",
+        "Mongolia",
+        "Montenegro",
+        "Montserrat",
+        "Morocco",
+        "Mozambique",
+        "Namibia",
+        "Nepal",
+        "Netherlands",
+        "New Caledonia",
+        "New Zealand",
+        "Nicaragua",
+        "Niger",
+        "Nigeria",
+        "Northern Mariana Islands",
+        "Norway",
+        "Oman",
+        "Pakistan",
+        "Palau",
+        "Panama",
+        "Papua New Guinea",
+        "Paraguay",
+        "Peru",
+        "Philippines",
+        "Poland",
+        "Ponape",
+        "Portugal",
+        "Puerto Rico",
+        "Qatar",
+        "Reunion",
+        "Romania",
+        "Rota",
+        "Russia",
+        "Rwanda",
+        "Saba (Netherlands Antilles)",
+        "Saipan",
+        "Samoa",
+        "Saudi Arabia",
+        "Senegal",
+        "Serbia",
+        "Seychelles",
+        "Sierra Leone",
+        "Singapore",
+        "Slovakia",
+        "Slovenia",
+        "Solomon Islands",
+        "South Africa",
+        "South Korea",
+        "Spain",
+        "Sri Lanka",
+        "St. Barthelemy",
+        "St. Croix",
+        "St. Eustatius (Netherlands Antilles)",
+        "St. John",
+        "St. Kitts and Nevis",
+        "St. Lucia",
+        "St. Maarten (Netherlands Antilles)",
+        "St. Thomas",
+        "St. Vincent and the Grenadines",
+        "Suriname",
+        "Swaziland",
+        "Sweden",
+        "Switzerland",
+        "Syria",
+        "Taiwan",
+        "Tajikistan",
+        "Tanzania",
+        "Thailand",
+        "Tinian",
+        "Togo",
+        "Tonga",
+        "Tortola",
+        "Trinidad and Tobago",
+        "Truk",
+        "Tunisia",
+        "Turkey",
+        "Turkmenistan",
+        "Turks and Caicos",
+        "Tuvalu",
+        "US Virgin Islands",
+        "Uganda",
+        "Ukraine",
+        "Union Island",
+        "United Arab Emirates",
+        "United Kingdom",
+        "United States",
+        "Uruguay",
+        "Uzbekistan",
+        "Vanuatu",
+        "Venezuela",
+        "Vietnam",
+        "Virgin Gorda",
+        "Wallis and Futuna",
+        "Yap",
+        "Yemen",
+        "Zambia",
+        "Zimbabwe"
+    ];
+})();
